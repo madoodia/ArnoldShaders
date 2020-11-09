0bcac5c1525c1b61d689289cba5793de44461f31
Reza Aarabi <madoodia@gmail.com>
2020-11-08 20:38:59 -0800

    trying to setup debug in windows, failed

--- build_scripts/build_debug.bat
+++ build_scripts/build_debug.bat
@@ -22,12 +22,9 @@ set VCVARS_LOCATION="C:/Program Files (x86)/Microsoft Visual Studio/2019/Communi
 call %VCVARS_LOCATION%/vcvarsall.bat x64
 
 @REM compile
-cl -c %ROOT%/%FOLDER%/%FILE_NAME%.cpp -I. -I%SDKS_LOCATION%/Arnold-6.1.0.0/include -DUNICODE -DWIN32 -D_WIN64 -D_WINDOWS -MT -EHsc /Fo:%ROOT%/%FOLDER%/%FILE_NAME%.obj
+cl -c %ROOT%/%FOLDER%/%FILE_NAME%.cpp /I. /I%SDKS_LOCATION%/Arnold-6.1.0.0/include /DUNICODE /DWIN32 /D_WIN64 /D_WINDOWS /MT /EHsc /O2 /FC /Zi /Fo:%ROOT%/%FOLDER%/%FILE_NAME%.obj
 
 @REM link
 link %ROOT%/%FOLDER%/%FILE_NAME%.obj /out:%ROOT%/%FOLDER%/%FILE_NAME%.dll /nologo /machine:x64 /dll %SDKS_LOCATION%/Arnold-6.1.0.0/lib/ai.lib kernel32.lib user32.lib gdi32.lib winspool.lib shell32.lib ole32.lib oleaut32.lib uuid.lib comdlg32.lib advapi32.lib
 
-@REM delete extra files
-del /F %ROOT%\%FOLDER%\%FILE_NAME%.lib
-del /F %ROOT%\%FOLDER%\%FILE_NAME%.exp 
-del /F %ROOT%\%FOLDER%\%FILE_NAME%.obj
+devenv %ROOT%/%FOLDER%/%FILE_NAME%.dll
